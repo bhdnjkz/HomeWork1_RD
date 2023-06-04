ab2cbbeeb248c601d7526c7b2a76bcf1c59e3bb9
Богдан <norestwithwest@Overlord.local>
2023-06-04 14:25:42 +0300

    Second ASCII art

--- HomeWork1_RD/Program.cs
+++ HomeWork1_RD/Program.cs
@@ -1,13 +1,19 @@
-﻿Console.WriteLine("          |");
-Console.WriteLine("          |   .");
-Console.WriteLine("   `.  *  |     .'");
-Console.WriteLine("     `. ._|_* .'  .");
-Console.WriteLine("   . * .'   `.  *");
-Console.WriteLine("-------|     |-------");
-Console.WriteLine("   .  *`.___.' *  .");
-Console.WriteLine("      .'  |* `.  *");
-Console.WriteLine("    .' *  |  . `.");
-Console.WriteLine("        . |");
-Console.WriteLine("          | ");
+﻿Console.WriteLine("         _nnnn_                      ");
+Console.WriteLine("        dGGGGMMb     ,\"\"\"\"\"\"\"\"\"\"\"\"\"\"\".");
+Console.WriteLine("       @p~qp~~qMb    | Linux Rules! |");
+Console.WriteLine("       M|@||@) M|   _;..............'");
+Console.WriteLine("       @,----.JM| -'");
+Console.WriteLine("      JS^\\__/  qKL");
+Console.WriteLine("     dZP        qKRb");
+Console.WriteLine("    dZP          qKKb");
+Console.WriteLine("   fZP            SMMb");
+Console.WriteLine("   HZM            MMMM");
+Console.WriteLine("   FqM            MMMM");
+Console.WriteLine(" __| \".        |\\dS\"qML");
+Console.WriteLine(" |    `.       | `' \\Zq");
+Console.WriteLine("_)      \\.___.,|     .'");
+Console.WriteLine("\\____   )MMMMMM|   .'");
+Console.WriteLine("     `-'       `--'   ");
+
 
 Console.ReadLine();
