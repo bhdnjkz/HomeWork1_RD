756dda228baaa4aac516fd08f45101b7ddbcb006
Богдан <norestwithwest@Overlord.local>
2023-06-04 14:16:51 +0300

    first ASCII art

---
+++ HomeWork1_RD/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("          |");
+Console.WriteLine("          |   .");
+Console.WriteLine("   `.  *  |     .'");
+Console.WriteLine("     `. ._|_* .'  .");
+Console.WriteLine("   . * .'   `.  *");
+Console.WriteLine("-------|     |-------");
+Console.WriteLine("   .  *`.___.' *  .");
+Console.WriteLine("      .'  |* `.  *");
+Console.WriteLine("    .' *  |  . `.");
+Console.WriteLine("        . |");
+Console.WriteLine("          | ");
+
+Console.ReadLine();
